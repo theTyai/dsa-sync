089204ed0d62710ba359fcf9ab9b1da85b57e4cd
theTyai <work.mishraashish@gmail.com>
2026-05-14 19:22:48 +0545

    ✅ Add LeetCode #2553: Separate the Digits in an Array (javascript)

---
+++ LeetCode/2553-Separate-the-Digits-in-an-Array/solution.js
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+        for(int x:nums)
+        {
+            while(x!=0)
+        }
+            {
+                int d=x%10;
+            }
+    }
+                a.push_back(d);
+                x/=10;
+        return ans;
+
+            vector<int> a;
+            for(auto x=rend()
+};
